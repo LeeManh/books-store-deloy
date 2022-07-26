7fcf81d98c37b6bc732c4d35fd856644e8f7f682
LeeManh <lemanhddt@gmail.com>
2022-07-26 21:21:46 +0700

    change api

--- src/api/booksApi.js
+++ src/api/booksApi.js
@@ -1,12 +1,12 @@
 import axios from "./clientAxios";
 
 const getAllBooks = async () => {
-  const url = "products";
+  const url = "books";
   return await axios.get(url);
 };
 
 const getBookById = async (idBook) => {
-  const url = `products/${idBook}`;
+  const url = `books/${idBook}`;
   return await axios.get(url);
 };
 
