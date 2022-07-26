3f8c9210843e93de9334576aefccebe23ad7cc62
LeeManh <lemanhddt@gmail.com>
2022-07-26 21:49:48 +0700

    deloy

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const jsonServer = require("json-server");
-const app = jsonServer.create();
-const path = require("path");
-const express = require("express");
-const middlewares = jsonServer.defaults();
-const router = jsonServer.router("db.json");
-const port = process.env.PORT || 3500;
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
 
-app.use("/db", middlewares, router);
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+const middlewares = jsonServer.defaults({
+  static: "./build",
 });
+const PORT = process.env.PORT || 3500;
+server.use(middlewares);
 
-server.listen(port);
+server.use(router);
+server.listen(PORT, () => {
+  console.log("Server is running");
+});
